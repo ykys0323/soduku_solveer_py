46b29d21033386fee0c6f6a52bbc9c5ea2d9248e
beh <behyuankuan1995@gmail.com>
2024-03-24 23:46:33 +0800

    Add function to add only possible within a row/col/3x3

--- solver.py
+++ solver.py
@@ -12,6 +12,7 @@ class SudokuSolver():
         self.result = np.ndarray
         self.done_solve = False
         self.update_count = 0
+        self.all_index_possible = []
 
     #convert three time three array to 1 row
     def convert_to_three_x_three(self,c_sudoku):
@@ -38,6 +39,8 @@ class SudokuSolver():
     
 
     def update_sudoku(self,num,row_id,col_id):
+        if self.copy_sudoku[row_id][col_id] != 0:
+            print("Wasting power")
         self.update_count +=1 
         self.copy_sudoku[row_id][col_id] = num
         self.transpose_sudoku[col_id][row_id] = num
@@ -45,12 +48,51 @@ class SudokuSolver():
 
     def check_repeating(self):
         pass
-
+    def check_impossible_for_row(self,pattern,row_id):
+        for i in range(9):
+            count = 0
+            found_index = -1
+            for j in range(9):
+                if pattern == 1:
+                    if i+1 in self.all_index_possible[row_id][j]:
+                        if count == 0:
+                            count+=1
+                            found_index = j
+                        else:
+                            count+=1
+                elif pattern ==2:
+                    if i+1 in self.all_index_possible[j][row_id]:
+                        if count == 0:
+                            count+=1
+                            found_index = j
+                        else:
+                            count+=1
+                else:
+                    if i+1 in self.all_index_possible[j//3 + row_id//3 *3][row_id%3 * 3 + j % 3]:
+                        if count == 0:
+                            count+=1
+                            found_index = j
+                        else:
+                            count+=1
+            len_of_index = 0
+            if pattern == 1:
+                len_of_index = len(self.all_index_possible[row_id][found_index])
+            elif pattern == 2:
+                len_of_index = len(self.all_index_possible[found_index][row_id])
+            else:
+                len_of_index = len(self.all_index_possible[found_index//3 + row_id//3 *3][row_id%3 * 3 + found_index % 3])
+            if count == 1 and len_of_index > 1:
+                # if pattern == 3:
+                #     print(f"Found with row {row_id} col {found_index} value {i+1}")
+                #     print(row_id//3 *3 + found_index//3,found_index%3 + row_id%3 *3)
+                return True,found_index,i+1
+        return False,None,None
+            
     def find_possible_for_index(self,row_id,col_id):
         check_normal,normal_available,_ = self.check_row(self.copy_sudoku[row_id])
         check_transpose,transpose_available,_ = self.check_row(self.transpose_sudoku[col_id])
         check_three_x_three,three_x_three_available,_ = self.check_row(self.three_x_three_sudoku[col_id//3 + row_id//3 *3])
-        
+
         if check_normal and check_transpose and check_three_x_three:
             common_index = np.intersect1d(normal_available,np.intersect1d(transpose_available,three_x_three_available))
             return True,common_index
@@ -58,43 +100,69 @@ class SudokuSolver():
             return False,None
 
     def find_possible(self):
+        cant_find_any = True
+        self.all_index_possible = []
         for row_id in range(9):
+            temp_possible = []
             for col_id in range(9):
                 if self.copy_sudoku[row_id][col_id] != 0:
+                    temp_possible.append([np.array(self.copy_sudoku[row_id][col_id])])
                     continue
                 check_true,normal_possible = self.find_possible_for_index(row_id,col_id)
                 if check_true:
                     if len(normal_possible) == 1:
+                        cant_find_any = False
                         self.update_sudoku(normal_possible[0],row_id,col_id)
-                else:
-                    continue
+                temp_possible.append(normal_possible)
+
+            self.all_index_possible.append(temp_possible)
+        return cant_find_any
+    
+    def find_another_possible(self):
+        for row_id in range(9):
+            check_possible,col_id,num_value = self.check_impossible_for_row(1,row_id)
+            if check_possible:
+                self.update_sudoku(num_value,row_id,col_id)
+            check_possible,col_id,num_value = self.check_impossible_for_row(2,row_id)
+            if check_possible:
+                self.update_sudoku(num_value,col_id,row_id)
+            check_possible,col_id,num_value = self.check_impossible_for_row(3,row_id)
+            if check_possible:
+                self.update_sudoku(num_value, row_id//3 *3 + col_id//3,col_id%3 + row_id%3 *3)
+                pass
+
     def solve(self) -> np.ndarray:
         start_time = time.time()
         count_try = 0
         while not self.done_solve:
-            self.find_possible()
+            not_possible = self.find_possible()
             if not np.any(self.copy_sudoku == 0):
                 self.done_solve = True
                 self.result = self.copy_sudoku
                 print(f"Fill total {self.update_count}")
+            if not_possible:
+                self.find_another_possible()
+                # print("######")
             count_try += 1
-            if count_try == 10:
+            if count_try == 81:
                 print("Break due to out of search")
+                print(self.copy_sudoku)
+                # print(self.all_index_possible)
                 break
 
         end_time = time.time()
         print(f"Time taken to solve {end_time - start_time}")
         return self.result
 
-sudoku = np.array([[0,0,8,7,0,2,0,0,5],
-                   [0,9,0,4,0,6,1,0,0],
-                   [5,1,4,3,9,8,0,2,0],
-                   [0,0,1,6,3,0,8,7,0],
-                   [6,0,5,9,0,0,2,3,1],
-                   [8,0,9,1,0,7,5,4,0],
-                   [2,0,6,0,0,3,9,0,4],
-                   [0,5,3,2,4,0,7,6,0],
-                   [1,0,7,8,0,0,3,0,2]])
+sudoku = np.array( [[0,0,0,8,0,3,0,0,0],
+                    [0,0,9,0,4,0,2,0,0],
+                    [0,5,0,0,0,0,0,4,0],
+                    [0,9,0,0,0,0,4,0,0],
+                    [0,4,0,0,2,1,0,0,8],
+                    [1,0,6,0,0,8,0,0,0],
+                    [6,0,0,1,0,0,0,8,5],
+                    [0,0,0,0,0,0,0,0,0],
+                    [0,2,0,0,3,6,0,0,0]])
 
 if __name__ == '__main__':
     sudoku_solver = SudokuSolver(sudoku)
